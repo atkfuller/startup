32792c47dcffde619e9137b9bf710a5910d63e57
ashleyfuller <atkfuller@gmail.com>
2025-10-08 16:12:17 -0600

    creating a browser rotuer

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { AddEvent } from './addEvent/adddEvent';
+import { Calendar } from './calendar/calendar';
+import { Event } from './event/event';
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="body bg-light text-dark">
 
-      <main>App components go here</main>
+      <Routes>
+    <Route path='/' element={<Login />} exact />
+    <Route path='/play' element={<AddEvent />} />
+    <Route path='/scores' element={<Calendar />} />
+    <Route path='/about' element={<Event />} />
+    <Route path='*' element={<NotFound />} />
+  </Routes>
       <footer>
             <p>Created by <a href="https://github.com/atkfuller/startup.git" target="_blank">My GitHub</a></p>
         </footer>
     </div>
+    
+    </BrowserRouter>
   );
+  
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
