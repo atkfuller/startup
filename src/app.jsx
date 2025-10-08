98063d39fd7ac8f3fc6b413d0ae7d736ec531733
ashleyfuller <atkfuller@gmail.com>
2025-10-08 16:03:42 -0600

    edited app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-light text-dark">
+
+      <main>App components go here</main>
+      <footer>
+            <p>Created by <a href="https://github.com/atkfuller/startup.git" target="_blank">My GitHub</a></p>
+        </footer>
+    </div>
+  );
 }
