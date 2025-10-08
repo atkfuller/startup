53af128898880a56292ee042a065dfd099c8a26e
ashleyfuller <atkfuller@gmail.com>
2025-10-08 16:06:21 -0600

    added all the javascript files

---
+++ src/addEvent/eventform.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AddEvent() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>addevent displayed here</div>
+    </main>
+  );
+}
---
+++ src/event/event.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Event() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>events displayed here</div>
+    </main>
+  );
+}
