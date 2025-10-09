05b7220b728b27839f8e6809e9c2aa298653732b
ashleyfuller <atkfuller@gmail.com>
2025-10-08 19:06:57 -0600

    trying something

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -13,39 +13,68 @@ export default function Login() {
   };
 
   return (
-    <div className="login-page">
+     <div>
       <h1>ULTIMATE CALENDAR</h1>
-
       <main>
-        <div className="main-body">
         <h1>Sign In</h1>
-
         <form onSubmit={handleSubmit}>
-          <div className="input-group">
+           <div className="input-group">
             <span>@</span>
-            <input
-              type="text"
-              placeholder="your@email.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+             <input
+           type="text"
+               placeholder="your@email.com"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
               required
-            />
-          </div>
-
-          <div className="input-group">
+             />
+           </div>
+            <div className="input-group">
             <span>🔒</span>
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-        </div> 
+             <input
+               type="password"
+               placeholder="password"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               required
+             />
+         </div> 
           <button type="submit">Login</button>
         </form>
-        </div>.
       </main>
     </div>
+    // <div className="login-page">
+    //   <h1>ULTIMATE CALENDAR</h1>
+
+    //   <main>
+    //     <div className="main-body">
+    //     <h1>Sign In</h1>
+
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="input-group">
+    //         <span>@</span>
+    //         <input
+    //           type="text"
+    //           placeholder="your@email.com"
+    //           value={email}
+    //           onChange={(e) => setEmail(e.target.value)}
+    //           required
+    //         />
+    //       </div>
+
+    //       <div className="input-group">
+    //         <span>🔒</span>
+    //         <input
+    //           type="password"
+    //           placeholder="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //           required
+    //         />
+    //     </div> 
+    //       <button type="submit">Login</button>
+    //     </form>
+    //     </div>.
+    //   </main>
+    // </div>
   );
 }
