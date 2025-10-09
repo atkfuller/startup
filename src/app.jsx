7d97a8c65f9b4d9ea66cd0dfd2e48780dc91a08a
ashleyfuller <atkfuller@gmail.com>
2025-10-09 11:37:27 -0600

    edited app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import Login  from './login/login.jsx';
+import AddEvent from './addEvent/eventform.jsx';
+import Calendar from './calendar/calendar.jsx';
+import  Event  from './event/event.jsx';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <BrowserRouter>
+    <div className='body'>
+      <Routes>
+    <Route path='/' element={<Login />} exact />
+    <Route path='/addEvent' element={<AddEvent />} />
+    <Route path='/calendar' element={<Calendar />} />
+    <Route path='/event' element={<Event />} />
+    <Route path='*' element={<NotFound />} />
+  </Routes>
+      <footer>
+            <p>Created by <a href="https://github.com/atkfuller/startup.git" target="_blank">My GitHub</a></p>
+        </footer>
+    </div>
+    </BrowserRouter>
+  );
+  
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
