094413961055180aa4f1bce56c25df408dafcdf6
ashleyfuller <atkfuller@gmail.com>
2025-10-14 14:17:03 -0600

    estabilishing the weeks in the weeks

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import "./calendar.css";
 import { useNavigate } from "react-router-dom";
 
-export default function Calendar() {
+export default function Calendar({events}) {
   const navigate = useNavigate();
+  const today= new Date();
+  const year= today.getFullYear();
+  const month= today.getMonth();
+  const monthNames= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const firstDay= new Date(year, month, 1).getDay();
+  const daysInMonth= new Date(year, month + 1, 0).getDate();
+  const weeks=[];
+  let day = 1-firstDay.getDay();
+  //geneartes the weeks in the calendar for the month
+  while(day<=daysInMonth){
+    const week=[];
+    for(let i=0; i<7; i++){
+      if(day>0 && day<=daysInMonth){
+        week.push(day);
+      } else {
+        week.push(null);
+      }
+      day++;
+    } 
+    weeks.push(week);
+  }
+ const getEventsForDay = (dayNum) => {
+  if(!dayNum) return [];
+    const date = `${year}-${String(month + 1).padStart(2, "0")}-${String(dayNum).padStart(2, "0")}`;
+    return events.filter(ev => ev.date === date);
+ }
+
+  
   return (
     <div className="calendar-page">
       <h1>Welcome to Ultimate Calendar</h1>
